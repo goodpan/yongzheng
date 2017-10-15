ba214d92eb0ce0c55fba09f61773435a22b3c38a
goodpan <suwen0603@163.com>
2017-10-15 14:11:45 +0800

    上传vendor

---
+++ pc/config/main-local.php
@@ -0,0 +1,26 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => '7l-_YgDTsIIU7693wW5prz4uk-d7c83P',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        'allowedIPs'=>['127.0.0.1']
+    ];
+}
+
+return $config;
