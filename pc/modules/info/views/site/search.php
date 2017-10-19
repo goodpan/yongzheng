00045f08a9ed711062a47d48c1cb3ded98d901db
wenzhen-chen <18259656089@163.com>
2017-10-19 12:13:38 +0800

    提交搜索页面

---
+++ pc/modules/info/views/site/search.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * 搜索页面
+ * Created by PhpStorm.
+ * User: chenwenzhen
+ * Date: 2017/10/19
+ * Time: 11:55
+ */
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    dfad
+</body>
+</html>
