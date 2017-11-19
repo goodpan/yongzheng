916b69883a430644760df6613f8244de259bc6dd
wenzhen-chen <18259656089@163.com>
2017-11-19 11:45:15 +0800

    main-local.php 增加

---
+++ common/config/main-local.php
@@ -0,0 +1,31 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=47.94.161.223;dbname=yongzheng',
+            'username' => 'root',
+            'password' => 'liu5555',
+            'charset' => 'utf8',
+            'tablePrefix'=>'yz_'
+        ],
+        'request' => [
+            'cookieValidationKey' => 'fcuVvgFv0Vex88Qm5N2-h6HH5anM4HEd',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.163.com',
+                'username' => 'suwen0603@163.com',//授权邮箱
+                'password' => 'yongzheng2017',//授权码，在163邮箱中设置--POP3/SMTP/IMAP，打开服务设置授权码
+                'port' => '465',
+                'encryption' => 'ssl',
+            ],
+        ],
+    ],
+];
