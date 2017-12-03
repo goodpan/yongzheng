f82d5db40fb0ab492fae699def1126b41aadc2d4
lmk <894580582@qq.com>
2017-12-03 14:20:56 +0800

    lmk

--- pc/config/main-local.php
+++ pc/config/main-local.php
@@ -16,18 +16,4 @@ $config = [
     ],
 ];
 
-if (!YII_ENV_TEST) {
-    // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => 'yii\debug\Module',
-    ];
-
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-        'allowedIPs'=>['127.0.0.1']
-    ];
-}
-
 return $config;
