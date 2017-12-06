8b40eb80441f3964fa95919646c989bb9f12f5c4
goodpan <goodpannet@163.cpm>
2017-12-06 08:44:09 +0800

    wap入口及配置文件

---
+++ wap/config/main-local.php
@@ -0,0 +1,41 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            'cookieValidationKey' => 'fcuVvgFv0Vex88Qm5N2-h6HH5anM4HEd',
+        ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=47.94.161.223;dbname=yongzheng',
+            'username' => 'root',
+            'password' => 'liu5555',
+            'charset' => 'utf8',
+            'tablePrefix' => 'yz_'
+        ],
+        // 'db' => [
+        //     'class' => 'yii\db\Connection',
+        //     'dsn' => 'mysql:host=127.0.0.1;dbname=yongzheng',
+        //     'username' => 'root',
+        //     'password' => 'root',
+        //     'charset' => 'utf8',
+        //     'tablePrefix' =>'yz_',
+        // ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        'allowedIPs'=>['127.0.0.1']
+    ];
+}
+
+return $config;
