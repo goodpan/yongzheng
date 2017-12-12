88d7f4ec5558ce38cb7f0ed1d7b418d0b288416c
wenzhen-chen <18259656089@163.com>
2017-12-12 20:38:35 +0800

    修改配置

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -1,5 +1,6 @@
 <?php
 Yii::setAlias('@common', dirname(__DIR__));
 Yii::setAlias('@pc', dirname(dirname(__DIR__)) . '/pc');
+Yii::setAlias('@wap', dirname(dirname(__DIR__)) . '/wap');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
--- common/config/main-local.php
+++ common/config/main-local.php
@@ -1,34 +1,44 @@
 <?php
-return [
-    'components' => [
-        'db' => [
-            'class' => 'yii\db\Connection',
-            //'dsn' => 'mysql:host=47.94.161.223;dbname=yongzheng',
-            //'username' => 'root',
-            //'password' => 'liu5555',
-            'dsn' => 'mysql:host=127.0.0.1;dbname=yongzheng',
-            'username' => 'root',
-            'password' => 'root',
-            'charset' => 'utf8',
-            'tablePrefix'=>'yz_'
-        ],
-        'request' => [
-            'cookieValidationKey' => 'fcuVvgFv0Vex88Qm5N2-h6HH5anM4HEd',
-        ],
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => false,
-            'transport' => [
-                'class' => 'Swift_SmtpTransport',
-                'host' => 'smtp.163.com',
-                'username' => 'suwen0603@163.com',//ÊÚÈ¨ÓÊÏä
-                'password' => 'yongzheng2017',//ÊÚÈ¨Âë£¬ÔÚ163ÓÊÏäÖÐÉèÖÃ--POP3/SMTP/IMAP£¬´ò¿ª·þÎñÉèÖÃÊÚÈ¨Âë
-                'port' => '465',
-                'encryption' => 'ssl',
-            ],
+
+$config = [ 'components' => [
+    'request' => [
+        'cookieValidationKey' => 'fcuVvgFv0Vex88Qm5N2-h6HH5anM4HEd',
+    ],
+    'db' => [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=47.94.161.223;dbname=yongzheng',
+        'username' => 'root',
+        'password' => 'study123456',
+        'charset' => 'utf8',
+        'tablePrefix'=>'yz_'
+    ],
+    'mailer' => [
+        'class' => 'yii\swiftmailer\Mailer',
+        // send all mails to a file by default. You have to set
+        // 'useFileTransport' to false and configure a transport
+        // for the mailer to send real emails.
+        'useFileTransport' => false,
+        'transport' => [
+            'class' => 'Swift_SmtpTransport',
+            'host' => 'smtp.163.com',
+            'username' => 'suwen0603@163.com',//æŽˆæƒé‚®ç®±
+            'password' => 'yongzheng2017',//æŽˆæƒç ï¼Œåœ¨163é‚®ç®±ä¸­è®¾ç½®--POP3/SMTP/IMAPï¼Œæ‰“å¼€æœåŠ¡è®¾ç½®æŽˆæƒç 
+            'port' => '465',
+            'encryption' => 'ssl',
         ],
     ],
+    'elasticsearch' => [
+        'class' => 'yii\elasticsearch\Connection',
+        'nodes' => [
+            ['http_address' => '127.0.0.1:9200'],
+            // configure more hosts if you have a cluster
+        ],
+    ],
+],
 ];
+if (!YII_ENV_TEST) {  // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [  'class' => 'yii\debug\Module', 'allowedIPs' => [ '*'] ];
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [ 'class' => 'yii\gii\Module', ]; }
+return $config;
